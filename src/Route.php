e82b87b14cd4e4d3a3a5e72415183e762fe30a7f
koyuch <michal.kozuch@odbav.to>
2017-03-23 16:46:06 +0100

    match parameters in Route::match() method, made tests for it

--- src/Route.php
+++ src/Route.php
@@ -53,15 +53,29 @@ class Route implements IRouter
 		if (!$this->isHttpMethodSupported($httpRequest->getMethod())) {
 			return NULL;
 		}
+		
+		$route = $this->normalizePath($this->route);
+		$route = str_replace('/', '\/', $route);
+		
+		// use named subpatterns to match params
+		$routeRegex = preg_replace('/<[\w_-]+>/', '(?$0[\w_-]+)', 
+			$route);
+		$routeRegex = '@' . $routeRegex . '@';
 
-		if ($path !== $this->route) {
+		$result = preg_match($routeRegex, $path, $matches);
+		if (!$result) {
 			return NULL;
 		}
-
+		
+		$params = $httpRequest->getQuery();
+		if (is_array($matches) && count($matches) > 1) {
+			$params += array_filter($matches, 'is_string', ARRAY_FILTER_USE_KEY);
+		}
+		
 		return new Request(
 			$this->presenterClassName,
 			$httpRequest->getMethod(),
-			$httpRequest->getQuery(),
+			$params,
 			$httpRequest->getPost(),
 			$httpRequest->getFiles(),
 			[Request::SECURED => $httpRequest->isSecured()]
@@ -82,16 +96,14 @@ class Route implements IRouter
 	{
 		$baseUrl = $refUrl->getHostUrl();
 		
-		$path = preg_replace_callback('/<([\w]+)>/', function ($matches) use ($appRequest) {
-			foreach ($matches as $match) {
-				if ($match[0] === '<') {
-					continue;
-				}
-				if ($value = $appRequest->getParameter($match)) {
-					return $value;
-				} else {
-					throw new RouteException('Parameter ' . $match . ' is not defined in Request.');
-				}
+		$path = preg_replace_callback('/<([\w_-]+)>/', function ($matches) use ($appRequest)
+		{
+			$match = $matches[1];
+			$value = $appRequest->getParameter($match);
+			if ($value) {
+				return $value;
+			} else {
+				throw new RouteException('Parameter ' . $match . ' is not defined in Request.');
 			}
 		}, $this->route);
 		
--- tests/RouteTest.php
+++ tests/RouteTest.php
@@ -10,16 +10,17 @@ use Nette\Http\Request as HttpRequest;
 use Nette\Http\UrlScript;
 use Nette\Http\Url;
 use Nette\Application\Request as AppRequest;
+use OdbavTo\PresenterRoute\RouteException;
 
 class RouteTest extends TestCase
 {	
 	/**
-	 * 
+	 * @test
 	 * @dataProvider matchProvider
 	 * @param string $route
 	 * @param string $url
 	 */
-	public function match(string $route, string $url, bool $result)
+	public function match(string $route, string $url, bool $result, array $params = [])
 	{
 		$httpRequest = new HttpRequest(new UrlScript($url));
 		$presenter = 'presenter';
@@ -28,7 +29,7 @@ class RouteTest extends TestCase
 		$appRequest = new AppRequest(
 			$presenter,
 			$httpRequest->getMethod(),
-			$httpRequest->getQuery(),
+			$params,
 			$httpRequest->getPost(),
 			$httpRequest->getFiles(),
 			[AppRequest::SECURED => $httpRequest->isSecured()]
@@ -46,6 +47,19 @@ class RouteTest extends TestCase
 			['hele/mese', 'http://www.hele.cz/hele/mese/', true],
 			['hele/mese/', 'http://www.hele.cz/hele/mese/', true],
 			['hele/mese', 'http://www.hele.cz/hele/', false],
+			['he-le/mese/', 'http://www.hele.cz/he-le/mese/', true],
+			['he-le/mese/', 'http://www.hele.cz/hele/mese/', false],
+			['hele/mese/', 'http://www.hele.cz/he-le/mese/', false],
+			['hele/<id>', 'http://www.hele.cz/hele/21', true, ['id' => 21]],
+			['hele/<id>/', 'http://www.hele.cz/hele/21', true, ['id' => 21]],
+			['he-le/<id>', 'http://www.hele.cz/he-le/21', true, ['id' => 21]],
+			['hele/<id>', 'http://www.hele.cz/hele/mese', true, ['id' => 'mese']],
+			['hele/<id>', 'http://www.hele.cz/hele/me3se', true, ['id' => 'me3se']],
+			['hele/<id>', 'http://www.hele.cz/hele/me-se', true, ['id' => 'me-se']],
+			['hele/<i_d>', 'http://www.hele.cz/hele/me-se', true, ['i_d' => 'me-se']],
+			['hele/<id>/mese', 'http://www.hele.cz/hele/21', false],
+			['hele/<id>/mese', 'http://www.hele.cz/hele/21/mese', true, ['id' => 21]],
+			['hele/<id>/<pid>', 'http://www.hele.cz/hele/123/456', true, ['id' => 123, 'pid' => 456]],
 		];
 	}
 	
@@ -55,7 +69,7 @@ class RouteTest extends TestCase
 	 * @param string $route
 	 * @param string $refUrl
 	 */
-	public function constructUrl(string $route, array $params, string $refUrl, string $expected)
+	public function constructUrl(string $route, array $params, string $refUrl, string $expected, string $exception = '')
 	{
 		$presenter = 'presenter';
 		
@@ -64,6 +78,10 @@ class RouteTest extends TestCase
 			IRequest::GET,
 			$params
 		);
+		
+		if ($exception) {
+			$this->expectException($exception);
+		}
 		
 		$return = (new Route($route, $presenter, [IRequest::GET] ))->constructUrl($appRequest, new Url($refUrl));
 		
@@ -76,10 +94,16 @@ class RouteTest extends TestCase
 			['hele/mese', [], 'http://www.hele.cz/hele/', 'http://www.hele.cz/hele/mese'],
 			['hele/mese/', [], 'http://www.hele.cz/hele/', 'http://www.hele.cz/hele/mese/'],
 			['hele/mese', [], 'http://www.hele.cz/hele', 'http://www.hele.cz/hele/mese'],
+			['hele/me-se', [], 'http://www.hele.cz/hele', 'http://www.hele.cz/hele/me-se'],
+			['hele/<id>', [], 'http://www.hele.cz/hele', 'http://www.hele.cz/hele/mese', RouteException::class],
 			['hele/<id>', ['id' => 123], 'http://www.hele.cz/hele/', 'http://www.hele.cz/hele/123'],
+			['hele/<i-d>', ['i-d' => 123], 'http://www.hele.cz/hele/', 'http://www.hele.cz/hele/123'],
+			['hele/<id>', ['id' => 'mese'], 'http://www.hele.cz/hele/', 'http://www.hele.cz/hele/mese'],
+			['hele/<id>', ['id' => 'me-se'], 'http://www.hele.cz/hele/', 'http://www.hele.cz/hele/me-se'],
 			['hele/<id>/', ['id' => 123], 'http://www.hele.cz/hele/', 'http://www.hele.cz/hele/123/'],
 			['hele/<id>/ok', ['id' => 123], 'http://www.hele.cz/hele/', 'http://www.hele.cz/hele/123/ok'],
 			['hele/<id>/<pid>', ['id' => 123, 'pid' => 456], 'http://www.hele.cz/hele/', 'http://www.hele.cz/hele/123/456'],
+			['hele/<id>/<pid>', ['id' => 123], 'http://www.hele.cz/hele/', 'http://www.hele.cz/hele/123/456', RouteException::class],
 		];
 	}
 }
